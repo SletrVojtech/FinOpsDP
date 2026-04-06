1874daa16c6ce61f7f0cc2eeb980527957e8c31b
Vojtěch Šletr <sletrvoj@fit.cvut.cz>
2026-04-06 16:44:34 +0200

    [webapp] Interface redesign

--- webapp/main.py
+++ webapp/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from routers import api_router, web_router
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = FastAPI(title="FinOps Platform")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # adding the sub-routers
 app.include_router(api_router.router)
